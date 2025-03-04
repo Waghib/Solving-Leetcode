044d49e73cc7eb5b7a83ef6e495302167932fa7d
Waghib <Waghibahmad30@gmail.com>
2025-03-04 23:24:06 +0500

    solving neetcode - contains dublicate

---
+++ contains-dublicate.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        
+    }
+}
+
+const solution = new Solution();
+console.log(solution.hasDuplicate([1, 2, 3, 1])); // true
+console.log(solution.hasDuplicate([1, 2, 3, 4])); // false
