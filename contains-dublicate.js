f4f330e2ac40d1b85fc07a4bce2812e5611b2dff
Waghib <Waghibahmad30@gmail.com>
2025-03-15 00:52:55 +0500

    solved contains-dublicate problem using hash set approach

--- contains-dublicate.js
+++ contains-dublicate.js
@@ -4,10 +4,18 @@ class Solution {
      * @return {boolean}
      */
     hasDuplicate(nums) {
-        
+        const seen = new Set();
+        for (const num of nums) {
+            if (seen.has(num)) {
+                return true;
+            }
+            seen.add(num);
+       }
+        return false;
     }
 }
 
 const solution = new Solution();
+console.log(solution.hasDuplicate([1, 2, 2, 3])); // true
 console.log(solution.hasDuplicate([1, 2, 3, 1])); // true
 console.log(solution.hasDuplicate([1, 2, 3, 4])); // false
